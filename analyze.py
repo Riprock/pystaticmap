4770982b985f5c9037fac7f0c142a661b2d778b7
Evan <8990377+Riprock@users.noreply.github.com>
2025-12-20 23:13:59 -0500

    Adding some recursive elements Since this is just a tree of nodes. Im doing a bit of recursion to be able to dig deep into the AST to build out the call tree

--- analyze.py
+++ analyze.py
@@ -1,8 +1,33 @@
 import ast
+master_data ={"functions": [], "expressions": [], "ifs": []}
 
+
+def node_iterator(nodes):
+    global master_data
+    for node in nodes:
+        if isinstance(node, ast.FunctionDef):
+            print(f"Function name: {node.name}")
+            print(f"Arguments: {[arg.arg for arg in node.args.args]}\n")  
+            master_data["functions"].append(node.name)
+            node_iterator(ast.iter_child_nodes(node))
+
+        elif isinstance(node, ast.Expr):
+            print(f"Node Value: {node.value}")
+            master_data["expressions"].append(ast.dump(node.value))
+            node_iterator(ast.iter_child_nodes(node))
+        
+        elif isinstance(node, ast.If):
+            print(f"Type {node.test}\n")
+            master_data["ifs"].append(ast.dump(node.test))
+            print(len(node.body))
+            print("END IF BODY")
+            node_iterator(ast.iter_child_nodes(node))
+        else:
+            print(f"NO Node type matched {type(node)}\n")
+call_tree = []
 with open("./tests/test.py", "r") as file:
     content = file.read()
-    tree = ast.parse(content, filename="./tests/test.py")
+    root = ast.parse(content, filename="./tests/test.py")
     #print(ast.dump(tree, indent=2))
 
     #walker = ast.walk(tree)
@@ -22,14 +47,8 @@ with open("./tests/test.py", "r") as file:
     <ast.FunctionDef object at 0x0000025FA67C5550>
     <ast.If object at 0x0000025FA67C4E10>
     """
-    for node in ast.iter_child_nodes(tree):
-        #print(node)
-        if isinstance(node, ast.FunctionDef):
-            print(f"Function name: {node.name}")
-            print(f"Arguments: {[arg.arg for arg in node.args.args]}\n")  
 
-        elif isinstance(node, ast.Expr):
-            print(f"Node Value: {node.value}\n")
-        
-        elif isinstance(node, ast.If):
-            print(f"Type {node.test}\n")
+    node_iterator(ast.iter_child_nodes(root))
+
+#print(master_data)
+#print("Call Tree")
